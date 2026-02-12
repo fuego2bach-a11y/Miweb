a9535f2e06400fa20952fe51ce0f21bf2f82096a
Pepo <fuego2bach@gmail.com>
2026-02-12 10:01:21 +0100

    seguridad.js

--- seguridad.js
+++ seguridad.js
@@ -1,14 +1,3 @@
 function pedirClave() {
-    let clave = prompt("Introduce la contraseña:");
-
-    // Validación oculta
-    if (validarClave(clave)) {
-        window.location.href = "privado.html";
-    } else {
-        alert("Contraseña incorrecta");
-    }
-}
-
-function validarClave(input) {
-    return btoa(input) === "MTMyNA==";
+    window.location.href = "login.html";
 }
